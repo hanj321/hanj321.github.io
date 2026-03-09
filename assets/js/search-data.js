a5ef65892c4c8ebbbd58301f1aa2a9041bdae74d
hanj321 <98137676+hanj321@users.noreply.github.com>
2026-03-09 21:57:44 +0000

    Deploying to gh-pages from @ hanj321/hanj321.github.io@5b70a0140164f28c0d2050579520f34d3884fdcf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-hanjun-park-joined-texas-tech-university-as-an-assistant-professor-in-the-department-of-industrial-manufacturing-and-systems-engineering-the-park-lab-was-established-to-advance-research-in-human-centered-systems-physical-ergonomics-and-occupational-biomechanics",
+          title: 'Hanjun Park joined Texas Tech University as an Assistant Professor in the Department...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
